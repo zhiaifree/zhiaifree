46783719ff7dfe5a786c6c6d5df6dc69185460c0
zhiaifree <1600845608@qq.com>
2025-03-26 22:17:01 +0800

    符号"/"去除

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,8 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zhiaifree.github.io/",
-  base: '/zhiaifree/',
+  site: "https://zhiaifree.github.io",
+  base: 'zhiaifree',
   integrations: [
     sitemap(),
     robotsTxt({
